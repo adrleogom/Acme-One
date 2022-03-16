fe235249ca5fe45dfb7a77dcd1a36b5a279e2cbc
Alvaro <alvesccas@alum.us.es>
2022-03-16 19:04:44 +0100

    Task 030: Fix

--- src/main/java/acme/entities/announcement/Annnouncement.java
+++ src/main/java/acme/entities/announcement/Annnouncement.java
@@ -43,7 +43,7 @@ public class Annnouncement {
 	protected Boolean flag;
 	
 	@URL
-	protected String link;
+	protected String furtherInfo;
 	
 	// Derived Attributes----------------------------------------------------------------------------
 	
