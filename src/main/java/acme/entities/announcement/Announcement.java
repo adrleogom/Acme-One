0e9230586cb81dfb318a6904521ade39ae4ebbf1
Alvaro <alvesccas@alum.us.es>
2022-03-20 13:02:18 +0100

    Task 030/R1: Fix.

--- src/main/java/acme/entities/announcement/Announcement.java
+++ src/main/java/acme/entities/announcement/Announcement.java
@@ -2,8 +2,10 @@ package acme.entities.announcement;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -12,6 +14,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
+import acme.framework.roles.Administrator;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -47,4 +50,8 @@ public class Announcement  extends AbstractEntity {
 	
 	
 	// Relationships---------------------------------------------------------------------------------
+		@NotNull
+		@Valid
+		@ManyToOne(optional = false)
+		protected Administrator administrator;
 }
