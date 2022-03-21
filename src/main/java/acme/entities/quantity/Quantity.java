cd594e9291ad507bdfecdb99fd61e989f0a63157
Adriana <adrianalg2000@hotmail.com>
2022-03-21 22:46:10 +0100

    Task 042/R1: Done.

--- src/main/java/acme/entities/quantity/Quantity.java
+++ src/main/java/acme/entities/quantity/Quantity.java
@@ -3,8 +3,8 @@ package acme.entities.quantity;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 import acme.entities.item.Item;
 import acme.entities.toolkit.Toolkit;
@@ -25,8 +25,8 @@ public class Quantity extends AbstractEntity{
 		// Attributes -------------------------------------------------------------
 	
 
-			@Positive
-			protected Integer number;
+			@Min(0)
+			protected int number;
 		
 		// Relationships ----------------------------------------------------------
 			@NotNull
