38f642e90cfdb0efb40e2d972c542687e72902da
Paula Ferreira Jiménez <paulaferreirajimenez@gmail.com>
2022-04-14 18:22:58 +0200

    Task-053:Done

--- src/main/java/acme/roles/Inventor.java
+++ src/main/java/acme/roles/Inventor.java
@@ -1,15 +1,11 @@
 package acme.roles;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.entities.item.Item;
 import acme.framework.roles.UserRole;
 import lombok.Getter;
 import lombok.Setter;
@@ -40,9 +36,5 @@ public class Inventor extends UserRole{
 
 	// Relationships ----------------------------------------------------------
 
-	@Valid
-	@NotNull
-	@ManyToOne(optional=false)
-	Item item;
-	
+
 }
