050c46747fd98aa3ef13e3f27c9dad1b1b7631ee
Abrcobgal <abrahamconil555@gmail.com>
2022-04-23 20:40:47 +0200

    Task 056-057/T2: Fixed.

--- src/test/java/acme/testing/inventor/patronage/InventorPatronageListTest.java
+++ src/test/java/acme/testing/inventor/patronage/InventorPatronageListTest.java
@@ -13,7 +13,7 @@ public class InventorPatronageListTest extends TestHarness {
 	@CsvFileSource(resources = "/inventor/patronage/patronage.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void positiveTestAuthenticated(final int recordIndex, final String code, final String status, final String initialDate, final String finalDate, final String legalStuff, final String budget, final String furtherInfo, final String company,
-		final String statement, final String pFurtherInformation, final String name, final String surname) {
+		final String statement, final String pFurtherInformation, final String name, final String surname, final String sNumber, final String creationMoment, final String memorandum, final String rFurtherInfo) {
 		super.signIn("inventor1", "inventor1");
 
 		super.clickOnMenu("Inventor", "List my patronages");
@@ -37,6 +37,19 @@ public class InventorPatronageListTest extends TestHarness {
 		super.checkInputBoxHasValue("patron.furtherInfo", pFurtherInformation);
 		super.checkInputBoxHasValue("patron.identity.name", name);
 		super.checkInputBoxHasValue("patron.identity.surname", surname);
+
+		super.clickOnButton("Patronage reports");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, sNumber);
+		super.checkColumnHasValue(recordIndex, 1, creationMoment);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("creationMoment", creationMoment);
+		super.checkInputBoxHasValue("memorandum", memorandum);
+		super.checkInputBoxHasValue("furtherInfo", rFurtherInfo);
 
 		super.signOut();
 	}
