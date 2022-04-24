20f9221f23ca1929557e43bcdbdc88f72d73f84c
Carmen <pr.carmengalvan@gmail.com>
2022-04-24 20:58:57 +0200

    Task 050/T1: Done.

---
+++ src/test/java/acme/testing/any/toolkit/AnyToolkitListPublishedTest.java
@@ -0,0 +1,39 @@
+package acme.testing.any.toolkit;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyToolkitListPublishedTest extends TestHarness{
+	
+	// Test cases -------------------------------------------------------------
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/toolkit/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String code, final String title,
+		final String description, final String assemblyNotes, final String furtherInfo,
+		final String retailPrice, final String inventor) {
+		
+
+		super.clickOnMenu("Anonymous", "List published toolkits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+
+		super.checkColumnHasValue(recordIndex, 0, code);
+		super.checkColumnHasValue(recordIndex, 1, title);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("assemblyNotes", assemblyNotes);
+		super.checkInputBoxHasValue("furtherInfo", furtherInfo);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		super.checkInputBoxHasValue("inventor", inventor);
+
+	}
+}
