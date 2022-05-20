860218417cb84eb1bb93bdb1c8045e8582d0c135
Alvaro <alvesccas@alum.us.es>
2022-05-21 00:59:20 +0200

    Task 076/T1: Done.

---
+++ src/test/java/acme/testing/inventor/toolkit/InventorToolkitPublishTest.java
@@ -0,0 +1,51 @@
+package acme.testing.inventor.toolkit;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class InventorToolkitPublishTest extends TestHarness{
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/toolkit/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String reference) {
+		super.signIn("inventor1", "inventor1");
+		
+		
+		super.clickOnMenu("Inventor", "List my toolkits");
+		super.checkListingExists();
+		
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(recordIndex, 0, reference);
+		
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.clickOnSubmit("Publish toolkit");
+		super.checkNotErrorsExist();
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/toolkit/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void negativeTest(final int recordIndex, final String reference) {
+		super.signIn("inventor1", "inventor1");
+		
+		
+		super.clickOnMenu("Inventor", "List my toolkits");
+		super.checkListingExists();
+		
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(recordIndex, 0, reference);
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.clickOnSubmit("Publish toolkit");
+		super.checkAlertExists(false);
+
+		super.signOut();
+	}
+
+}
